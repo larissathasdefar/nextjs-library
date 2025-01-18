c2f1e4d3e96c016b33efef31813485077ae6cb2a
Larissa Farias <larissathasdefar@hotmail.com>
2025-01-18 14:42:03 -0300

    feat: added custom theme files

---
+++ app/(theme)/AppTheme.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import type { ThemeOptions } from "@mui/material/styles";
+import { inputsCustomizations } from "./customizations/inputs";
+import { dataDisplayCustomizations } from "./customizations/dataDisplay";
+import { feedbackCustomizations } from "./customizations/feedback";
+import { navigationCustomizations } from "./customizations/navigation";
+import { surfacesCustomizations } from "./customizations/surfaces";
+import { colorSchemes, typography, shadows, shape } from "./themePrimitives";
+
+interface AppThemeProps {
+  children: React.ReactNode;
+  themeComponents?: ThemeOptions["components"];
+}
+
+// Using theme from https://mui.com/material-ui/getting-started/templates/
+export default function AppTheme(props: AppThemeProps) {
+  const { children, themeComponents } = props;
+  const theme = React.useMemo(() => {
+    return createTheme({
+      cssVariables: {
+        colorSchemeSelector: "data-mui-color-scheme",
+        cssVarPrefix: "template",
+      },
+      colorSchemes,
+      typography,
+      shadows,
+      shape,
+      components: {
+        ...inputsCustomizations,
+        ...dataDisplayCustomizations,
+        ...feedbackCustomizations,
+        ...navigationCustomizations,
+        ...surfacesCustomizations,
+        ...themeComponents,
+      },
+    });
+  }, [themeComponents]);
+  return (
+    <ThemeProvider theme={theme} disableTransitionOnChange>
+      {children}
+    </ThemeProvider>
+  );
+}
