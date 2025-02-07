b5d67cf093261a36cd6cbc33b8a2944a6f364c71
Larissa Farias <larissathasdefar@hotmail.com>
2025-02-06 22:50:12 -0300

    feat: reuse user form for creation and update

---
+++ app/admin/users/UserForm.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import Grid from "@mui/material/Grid2";
+import FormHelperText from "@mui/material/FormHelperText";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import { User } from "@/app/types/user";
+import PasswordInput from "@/app/components/PasswordInput";
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  padding: theme.spacing(4),
+}));
+
+const userTypes = [
+  {
+    value: "customer",
+    label: "Customer",
+  },
+  {
+    value: "employee",
+    label: "Employee",
+  },
+];
+
+type UserForm = {
+  user: User;
+  error?: void | undefined;
+  submitAction: () => void;
+  isPending: boolean;
+};
+
+export default function UserForm({
+  user,
+  error,
+  submitAction,
+  isPending,
+}: UserForm) {
+  return (
+    <Box
+      component="form"
+      action={submitAction}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "700px",
+      }}
+    >
+      <Card variant="outlined">
+        <Grid container spacing={2}>
+          <Grid size={8}>
+            <FormControl>
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <TextField
+                required
+                fullWidth
+                id="name"
+                name="name"
+                defaultValue={user.name}
+                placeholder="Name"
+              />
+            </FormControl>
+          </Grid>
+          <Grid size={4}>
+            <FormControl>
+              <FormLabel>User type</FormLabel>
+              <TextField
+                required
+                fullWidth
+                select
+                id="type"
+                name="type"
+                defaultValue={user.type || "customer"}
+              >
+                {userTypes.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </FormControl>
+          </Grid>
+          <Grid size={8}>
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                type="email"
+                name="email"
+                defaultValue={user.email}
+                placeholder="your@email.com"
+              />
+            </FormControl>
+          </Grid>
+          <Grid size={4} />
+          <Grid size={8}>
+            <FormControl sx={{ marginBottom: 1 }}>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <PasswordInput
+                fullWidth
+                name="password"
+                defaultValue={user.password}
+                placeholder="••••••"
+                id="password"
+              />
+              {user.id !== "" && (
+                <FormHelperText>
+                  This is an optional field. Fill this field to change the
+                  password.
+                </FormHelperText>
+              )}
+            </FormControl>
+          </Grid>
+          <Grid size={4} />
+          <Grid size={8} />
+          <Grid size={4} sx={{ display: "flex", justifyContent: "end" }}>
+            <Button
+              href="/admin/users"
+              variant="contained"
+              disabled={isPending}
+              sx={{ marginRight: 2 }}
+            >
+              Cancel
+            </Button>
+            <Button type="submit" variant="contained" disabled={isPending}>
+              Save
+            </Button>
+          </Grid>
+          {/* {error && <p>{error}</p>} */}
+        </Grid>
+      </Card>
+    </Box>
+  );
+}
--- app/admin/users/create/page.tsx
+++ app/admin/users/create/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+
+import { useActionState } from "react";
 import Typography from "@mui/material/Typography";
-// TODO: reuse form from edit to create user
+import UserForm from "@/app/admin/users/UserForm";
+
 export default function CreateUser() {
+  const [error, submitAction, isPending] = useActionState(() => {}, undefined);
   return (
     <div>
       <Typography variant="h4" sx={{ marginBottom: "40px" }}>
         Create user
       </Typography>
+
+      <UserForm
+        user={{
+          id: "",
+          name: "",
+          email: "",
+          type: "customer",
+          password: "",
+        }}
+        error={error}
+        submitAction={submitAction}
+        isPending={isPending}
+      />
     </div>
   );
 }
