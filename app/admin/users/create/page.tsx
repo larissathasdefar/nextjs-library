995cd45a8c647a4d36e20c7e6464b893f2a336b4
Larissa Farias <larissathasdefar@hotmail.com>
2025-02-06 22:10:15 -0300

    feat: added basic screen for create

---
+++ app/admin/users/create/page.tsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+// TODO: reuse form from edit to create user
+export default function CreateUser() {
+  return (
+    <div>
+      <Typography variant="h4" sx={{ marginBottom: "40px" }}>
+        Create user
+      </Typography>
+    </div>
+  );
+}
